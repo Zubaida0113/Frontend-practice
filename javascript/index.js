987269ccb6b583d6e9c95ecbf2b6d0360efc6cc5
Zubaida0113 <zubaidasehnaz@gmail.com>
2025-02-08 22:04:31 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,7 @@ console.log(c);
 
 console.log('Javascript hariables - var, const, let');
 var a=9
-var a=8
+var a=8 //var are global-scoped
 console.log(a) //output is 8 as var can be updated and re-assigned
 let b="harry"
 b=9
@@ -23,6 +23,6 @@ console.log(r) //output is zubaida as const can't be updated or re-assigned
 let t=9
 {
     let t=99
-    console.log(t)
+    console.log(t)//output is 99 as let and const are block-scoped
 }
 console.log(t)
