98bd77b39d2c28452f58be32990b3553b96269b1
Zubaida0113 <zubaidasehnaz@gmail.com>
2025-02-10 21:32:46 +0530

    Update string.js

--- javascript/string.js
+++ javascript/string.js
@@ -6,3 +6,8 @@ for(let i=0;i<a.length;i++){
     // console.log(a[i])
     process.stdout.write(a[i]+" ")
 }
+// template literals 
+const prompt = require('prompt-sync')();` `
+console.log(`You are my "special"`)
+let names =prompt("Enter name ")
+console.log(`My name is ${names}`)
