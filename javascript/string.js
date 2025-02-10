4aaa42090d12034ece96eb1453c0e241d7de1252
Zubaida0113 <zubaidasehnaz@gmail.com>
2025-02-11 00:06:25 +0530

    Update string.js

--- javascript/string.js
+++ javascript/string.js
@@ -11,3 +11,10 @@ const prompt = require('prompt-sync')();` `
 console.log(`You are my "special"`)
 let names =prompt("Enter name ")
 console.log(`My name is ${names}`)
+
+//escape sequence character 
+let n="It\'s just a \"joke\"."
+let nn='It\'s just a "joke".'
+console.log(n,nn)
+console.log(n.length)
+console.log(nn.length)
