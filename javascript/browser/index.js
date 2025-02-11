9a564a7f9e6961a00831e94a602c22f24346658a
Zubaida0113 <zubaidasehnaz@gmail.com>
2025-02-11 21:48:59 +0530

    dom manipulation

---
+++ javascript/browser/index.js
@@ -0,0 +1,62 @@
+// console.log(2+4)
+// console.log(console)
+
+// const obj={
+//     a:1, b:2,c:3,d:4
+// }
+// alert("Enter the number")
+// let a= prompt("Enter the number here")
+// a=Number.parseInt(a)
+// let c=typeof a
+// alert("You have entered a datatype of "+(c))
+
+// let w=confirm("Do you want to write?")
+// if(w){
+//     document.writeln(c)
+// }
+// else{
+//     document.writeln("No")
+// }
+// console.log(window)
+// console.log(document.body)
+// document.body.style.background="yellow"
+
+//ques 1,2,3
+// let runagain=true
+// while(runagain){
+//     alert("Enter your age")
+//     let a= prompt("Age")
+//     a=Number.parseInt(a)
+    
+//     if(a<18 && a>=0){
+//         alert("You can not drive")
+//     }
+//     else if(a<0){
+//         console.error("You have entered a negative number");
+//     }
+//     else{
+//         alert("You can drive")
+//     }
+//     runagain=confirm("Do you want to see the prompt again")
+//     //ok-true -> loop runs again / cancel-false -> loop ends
+// }
+
+// let n=(a<18)?"You can not drive":"You can drive"
+// alert(n)
+
+//ques 4
+// alert("Enter number")
+// let a= prompt("Number")
+// a=Number.parseInt(a)
+
+// if(a>4){
+//     location.href="https://www.google.com"
+// }
+// else{
+//     alert("number is less than 4")
+// }
+
+//ques 5
+let bg_color= prompt("Enter the background color")
+// document.body.style.background=`${bg_color}`
+document.body.style.background=bg_color
